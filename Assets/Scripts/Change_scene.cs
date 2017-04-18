ce30ac92d9444f3c30471ab61842da988fb4285b
jonkaz1 <j.kazlauskas@ktu.edu>
2017-04-18 16:56:36 +0300

    Bugo fixas kai dūmai neveikia
    Paspaudus: New game -> Random -> esc -> Exit dūmai sustodavo, nes
    neatsistatydavo timeScale nuo esc paspaudimo. Dabar keičiantis scenai,
    laikas atsistato į 1.

--- Assets/Scripts/Change_scene.cs
+++ Assets/Scripts/Change_scene.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class Change_scene : MonoBehaviour {
 
 	public void ChangeToScene (int sceneToChangeTo) {
+        Time.timeScale = 1;
         SceneManager.LoadScene(sceneToChangeTo);
     }
     public void ExitGame()
